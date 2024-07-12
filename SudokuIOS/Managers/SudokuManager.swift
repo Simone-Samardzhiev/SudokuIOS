445e57aa517ea3a90e350bddfaf975e45f97bbd0
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-12 23:15:24 +0300

    Added the SudokuNumber struct.

--- SudokuIOS/Managers/SudokuManager.swift
+++ SudokuIOS/Managers/SudokuManager.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+/// Struct used to create the numbers of the sudoku.
+struct SudokuNumber {
+    /// The value of the number.
+    var value: Int?
+    /// Bool variable representing if the number is generated or inputed by the user.
+    var isGenerated: Bool
+    /// Bool variable representing if the number should be generated.
+    var isHighlighted: Bool
+    
+    /// Default constructor.
+    init() {
+        self.value = nil
+        self.isGenerated = true
+        self.isHighlighted = false
+    }
+}
+
+/// Class that will manage the sudoku.
 class SudokuManager: ObservableObject {
     
 }
