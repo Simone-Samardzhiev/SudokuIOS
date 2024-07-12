e49395526afe3dfa96ceb3e4a24ad40461938915
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-12 22:39:56 +0300

    Initial Commit

---
+++ SudokuIOS/SudokuIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuIOSApp.swift
+//  SudokuIOS
+//
+//  Created by Simone Samardzhiev on 12.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
