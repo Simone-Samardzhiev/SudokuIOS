903d6fb8a77d813dc8b31f8c780af87283c84afe
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-12 22:45:31 +0300

    Added the stucture of the project by creating group Views and sub group Main Views where the main view is located. Also added the README file.

---
+++ SudokuIOS/Views/Main Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SudokuIOS
+//
+//  Created by Simone Samardzhiev on 12.07.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
