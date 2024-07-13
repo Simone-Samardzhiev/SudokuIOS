76e86269eef54e9bfe249b9f908fd1bb9be7adfc
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-13 12:16:00 +0300

    Added the struct SudokuView that will shwo the sudok.

---
+++ SudokuIOS/Views/Sudoku Views/SudokuView.swift
@@ -0,0 +1,21 @@
+//
+//  SudokuView.swift
+//  SudokuIOS
+//
+//  Created by Simone Samardzhiev on 13.07.24.
+//
+
+import SwiftUI
+
+
+/// Struct that will display the sudoku.
+struct SudokuView: View {
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+}
