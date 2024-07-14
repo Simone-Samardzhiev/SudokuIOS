e16c659ae799a48e1c219d2db5f363783386ab64
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-14 17:35:04 +0300

    Added the onSelectNumber method to each number in the number selection and improved the ui that displays the current selected number.

--- SudokuIOS/Views/Sudoku Views/NumberSelectionView.swift
+++ SudokuIOS/Views/Sudoku Views/NumberSelectionView.swift
@@ -26,11 +26,13 @@ struct NumberSelectionView: View {
             ForEach(1..<10) { number in
                 Spacer()
                 Button {
-                    
+                    sudokuManager.onSelectedNumber(newNumber: number)
                 } label: {
                     Text(String(number))
                         .foregroundStyle(Color.blue)
                         .font(Font.system(size: size / 10))
+                        .bold(number == sudokuManager.selectedNumber)
+                        .underline(number == sudokuManager.selectedNumber)
                 }
                 Spacer()
             }
