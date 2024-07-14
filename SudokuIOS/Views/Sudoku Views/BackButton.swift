2b66200541abdecc2b686fa449f3f914db8320bd
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-14 09:34:21 +0300

    Added the back button.

--- SudokuIOS/Views/Sudoku Views/BackButton.swift
+++ SudokuIOS/Views/Sudoku Views/BackButton.swift
@@ -5,4 +5,20 @@
 //  Created by Simone Samardzhiev on 13.07.24.
 //
 
-import Foundation
+import SwiftUI
+
+/// Button used to return form the main view
+struct BackButton: View {
+    /// The manager of the sudoku.
+    @EnvironmentObject var sudokuManager: SudokuManager
+
+    var body: some View {
+        Button {
+            sudokuManager.endGame()
+        } label: {
+            Image(systemName: "arrowshape.backward")
+                .foregroundStyle(Color.primary)
+        }
+
+    }
+}
