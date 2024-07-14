f4b016b41c80e09f5c69d9ed8633ed2f19572bf3
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-14 12:42:12 +0300

    Deleted the inputedColor, because we will use Color.blue

---
+++ SudokuIOS/Views/Sudoku Views/NumberSelectionView.swift
@@ -0,0 +1,17 @@
+//
+//  NumberSelection.swift
+//  SudokuIOS
+//
+//  Created by Simone Samardzhiev on 14.07.24.
+//
+
+import SwiftUI
+
+
+struct NumberSelection: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
