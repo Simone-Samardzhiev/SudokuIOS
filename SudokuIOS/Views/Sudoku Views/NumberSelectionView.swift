65338d58502b9a557fb657804f9f39cc425d4a81
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-14 12:48:15 +0300

    Added the number selection.

--- SudokuIOS/Views/Sudoku Views/NumberSelectionView.swift
+++ SudokuIOS/Views/Sudoku Views/NumberSelectionView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 
-struct NumberSelection: View {
+/// View used to select the numbers in the sudoku.
+struct NumberSelectionView: View {
+    /// The manager of the sudoku.
+    @EnvironmentObject var sudokuManager: SudokuManager
+    /// The width of the number selection.
+    let size: CGFloat
+    
+    /// Initialiser of the number selection view.
+    /// - Parameter size: The width of the number selection.
+    init(size: CGFloat) {
+        self.size = size
+    }
+    
     var body: some View {
         HStack {
-            
+            ForEach(1..<10) { number in
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text(String(number))
+                        .foregroundStyle(Color.blue)
+                        .font(Font.system(size: size / 10))
+                }
+                Spacer()
+            }
         }
+        .padding()
     }
 }
