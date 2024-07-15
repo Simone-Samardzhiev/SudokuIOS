3f0155980e772c0e19ef3b109734257934031477
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-15 08:36:30 +0300

    Created the new MistakesCounterView that will display the remaining mistakes.

--- SudokuIOS/Views/Sudoku Views/MistakesCounterView.swift
+++ SudokuIOS/Views/Sudoku Views/MistakesCounterView.swift
@@ -5,4 +5,21 @@
 //  Created by Simone Samardzhiev on 14.07.24.
 //
 
-import Foundation
+import SwiftUI
+
+
+/// View that will display the count of the mistakes.
+struct MistakesCounterView: View {
+    /// The manager of the sudoku.
+    @EnvironmentObject var sudokuManager: SudokuManager
+    
+    var body: some View {
+        HStack {
+            Text("Mistakes: \(sudokuManager.mistakesCount)")
+                .font(.title)
+                .foregroundStyle(Color.primary)
+            Spacer()
+        }
+        .padding()
+    }
+}
